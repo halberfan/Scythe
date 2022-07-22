b3a65d16873cd9e78b0562dd958744e69a417b5c
f.schmidt <feli.schmidt@ostfalia.de>
2022-07-22 16:50:48 +0200

    init

--- src/main/java/fts/sense/util/Recipes.java
+++ src/main/java/fts/sense/util/Recipes.java
@@ -1,8 +1,6 @@
 package fts.sense.util;
 
 import fts.sense.Sense;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.NBTComponentBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
